f5f7e05ce024296a460ac82831253acf9b654c24
Brandon Philips <brandon.philips@coreos.com>
2016-02-23 22:16:22 -0800

    cmd: improve the help output

--- cmd/echo.go
+++ cmd/echo.go
@@ -30,13 +30,7 @@ import (
 // echoCmd represents the echo command
 var echoCmd = &cobra.Command{
 	Use:   "echo",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Example echo gRPC service CLI client",
 	Run: func(cmd *cobra.Command, args []string) {
 		var opts []grpc.DialOption
 		creds := credentials.NewClientTLSFromCert(demoCertPool, "localhost:10000")
--- cmd/serve.go
+++ cmd/serve.go
@@ -26,13 +26,7 @@ import (
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Launches the example webserver on https://localhost:10000",
 	Run: func(cmd *cobra.Command, args []string) {
 		serve()
 	},
