859db53df24f0ade57a5d21e84be3bb4ffe6e01c
Oleg Smetanin <soa@inbox.ru>
2016-04-25 21:10:02 +0300

    Rebuild stub

--- echopb/service.pb.go
+++ echopb/service.pb.go
@@ -49,6 +49,10 @@ func init() {
 var _ context.Context
 var _ grpc.ClientConn
 
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion2
+
 // Client API for EchoService service
 
 type EchoServiceClient interface {
@@ -82,16 +86,22 @@ func RegisterEchoServiceServer(s *grpc.Server, srv EchoServiceServer) {
 	s.RegisterService(&_EchoService_serviceDesc, srv)
 }
 
-func _EchoService_Echo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _EchoService_Echo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(EchoMessage)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(EchoServiceServer).Echo(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(EchoServiceServer).Echo(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/echopb.EchoService/Echo",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(EchoServiceServer).Echo(ctx, req.(*EchoMessage))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 var _EchoService_serviceDesc = grpc.ServiceDesc{
